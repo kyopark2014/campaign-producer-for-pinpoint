1beeef7c9da8ee5b2ee7ec1b18d67f64104465f3
ksdyb.park <ksdyb@amazon.com>
2022-03-21 21:40:32 +0900

    update

--- bin/campaign-producer-for-pinpoint.ts
+++ bin/campaign-producer-for-pinpoint.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CampaignProducerForPinpointStack } from '../lib/campaign-producer-for-pinpoint-stack';
+import { CampaignProducerUsingPinpointStack } from '../lib/campaign-producer-for-pinpoint-stack';
 
 const app = new cdk.App();
-new CampaignProducerForPinpointStack(app, 'CampaignProducerForPinpointStack', {
+new CampaignProducerUsingPinpointStack(app, 'CampaignProducerForPinpointStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
